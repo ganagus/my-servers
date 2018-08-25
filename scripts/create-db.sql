8659077816edcd0a5c1b554522f5338a64cf4574
Agus Suhanto <agus.suhanto@gmail.com>
2018-08-25 08:06:46 +0700

    Add wordpress secret keys as parameter

--- scripts/create-db.sql
+++ scripts/create-db.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE wordpress;
+CREATE DATABASE aprojectguru;
 
-CREATE USER wordpress@'localhost' IDENTIFIED BY '<dbuserpassword>';
-CREATE USER wordpress@'%' IDENTIFIED BY '<dbuserpassword>';
+CREATE USER aprojectguru@'localhost' IDENTIFIED BY '<dbuserpassword>';
+CREATE USER aprojectguru@'%' IDENTIFIED BY '<dbuserpassword>';
 
-GRANT ALL PRIVILEGES ON wordpress.* TO wordpress@'localhost';
-GRANT ALL PRIVILEGES ON wordpress.* TO wordpress@'%';
+GRANT ALL PRIVILEGES ON aprojectguru.* TO aprojectguru@'localhost';
+GRANT ALL PRIVILEGES ON aprojectguru.* TO aprojectguru@'%';
