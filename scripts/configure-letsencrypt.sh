fec0b80a7e5e5e21b18a1bb2ef5f7a77aa5942a4
Agus Suhanto <agus.suhanto@gmail.com>
2018-08-26 08:13:14 +0700

    Letsencrypt script

---
+++ scripts/configure-letsencrypt.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# This sets up Let's Encrypt SSL certificates and automatic renewal 
+# using certbot: https://certbot.eff.org
+#
+# - Run this script as root.
+# - A webserver must be up and running.
+#
+# Certificate files are placed into subdirectories under
+# /etc/letsencrypt/live/*.
+# 
+# Configuration must then be updated for the systems using the 
+# certificates.
+#
+# The certbot-auto program logs to /var/log/letsencrypt.
+#
+ 
+set -o nounset
+set -o errexit
+
+# Configure letsencrypt certificates
+sudo mkdir /etc/letsencrypt
+sudo wget https://raw.githubusercontent.com/ganagus/my-servers/master/configs/letsencrypt/cli.ini -O /etc/letsencrypt/cli.ini
+sudo sed -i "s/<domainName>/$1/g" /etc/letsencrypt/cli.ini
+export HOME="/root"
+export PATH="${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+sudo wget https://dl.eff.org/certbot-auto
+sudo chmod a+x certbot-auto
+sudo mv certbot-auto /usr/local/bin
+sudo certbot-auto --noninteractive --os-packages-only
+sudo certbot-auto certonly
+
+# Configure letsencrypt renewal cron job
+sudo wget https://raw.githubusercontent.com/ganagus/my-servers/master/cron-jobs/certbot-renew.sh -O /etc/cron.daily/certbot-renew.sh
+sudo chmod a+x /etc/cron.daily/certbot-renew.sh
