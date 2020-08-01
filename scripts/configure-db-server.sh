297c3920b77f3e54464bd0a0a5c74a62693935e7
Agus Suhanto <agus.suhanto@gmail.com>
2020-08-01 17:04:29 +0700

    Bug fixing

--- scripts/configure-db-server.sh
+++ scripts/configure-db-server.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
 sudo apt-get update
 
 # Configure mysql-server
+sudo apt-get install -y debconf-utils
 export DEBIAN_FRONTEND="noninteractive"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $1"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $1"
-sudo apt-get install -y mysql-server
+sudo apt-get install -y mysql-server-5.7
+mysql_secure_installation
 sudo sed -i "s/= 127.0.0.1/= 0.0.0.0/g" /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo systemctl restart mysql
 
@@ -17,11 +18,11 @@ sudo sed -i "s/<dbPassword>/$3/g" create-db.sql
 sudo mysql -u"root" -p"$1" < create-db.sql
 
 # Install mongodb from mongodb-org
-# wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-# sudo apt-get update
-# sudo apt-get install -y mongodb-org
-# sudo systemctl start mongod
-# sudo systemctl enable mongod
+sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
 # add frontendIPAddress to list of allowed IPs
-#sudo sed -i "s/bind_ip = 127.0.0.1/bind_ip = 127.0.0.1,$4/g" /etc/mongod.conf
+sudo sed -i "s/= 127.0.0.1/= 0.0.0.0/g" /etc/mongod.conf
